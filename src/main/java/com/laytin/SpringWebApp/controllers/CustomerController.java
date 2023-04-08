cf4d4b499407276b13409330fd5c809ddfc9bde5
Laytin <xpor2002@gmail.com>
2023-04-08 21:34:08 +0300

    for test

--- src/main/java/com/laytin/SpringWebApp/controllers/CustomerController.java
+++ src/main/java/com/laytin/SpringWebApp/controllers/CustomerController.java
@@ -1,21 +1,24 @@
 package com.laytin.SpringWebApp.controllers;
 
 import com.laytin.SpringWebApp.models.Customer;
-import com.laytin.SpringWebApp.models.Ord;
-import com.laytin.SpringWebApp.models.OrdProduct;
 import com.laytin.SpringWebApp.services.CustomerService;
+import com.laytin.SpringWebApp.services.OrdService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
 @Controller
+@RequestMapping("/")
 public class CustomerController {
     private final CustomerService customerService;
+    private final OrdService ordService;
     @Autowired
-    public CustomerController(CustomerService customerService) {
+    public CustomerController(CustomerService customerService, OrdService ordService) {
         this.customerService = customerService;
+        this.ordService = ordService;
+    }
+    @GetMapping
+    public String index(){
+        return "hello";
     }
 }
