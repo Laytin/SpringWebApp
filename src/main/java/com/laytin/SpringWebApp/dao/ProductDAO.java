a02a606395ecd05acaca20a50be9ba36d52e7910
Laytin <xpor2002@gmail.com>
2023-04-13 00:04:32 +0300

    change hql to sql. We will test it

--- src/main/java/com/laytin/SpringWebApp/dao/ProductDAO.java
+++ src/main/java/com/laytin/SpringWebApp/dao/ProductDAO.java
@@ -1,9 +1,12 @@
 package com.laytin.SpringWebApp.dao;
 
+import com.laytin.SpringWebApp.models.Product;
+import org.hibernate.Session;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import javax.persistence.EntityManager;
+import java.util.List;
 
 @Component
 public class ProductDAO {
@@ -11,5 +14,15 @@ public class ProductDAO {
     @Autowired
     public ProductDAO(EntityManager entityManager) {
         this.entityManager = entityManager;
+    }
+    public List<Product> getSearchProducts(String searchRequest, int page, int perPage){
+        int start = 0+(page-1)*perPage;
+        int end = perPage*page;
+        Session session = entityManager.unwrap(Session.class);
+        List<Product> result  = session.createSQLQuery("SELECT " +
+                "(SELECT product.color,product.item_id FROM product p GROUP BY product.color,product.item_id) as productGroupSizes, " +
+                "JOIN ( Item on item_id = item.id)" +
+                "where item.name Like 'Nike%'\n").setFirstResult(start).setMaxResults(end).list();
+        return result;
     }
 }
