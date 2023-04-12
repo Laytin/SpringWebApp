71e78c8b871a9ab29a213d1e6ab2b266e1338c89
Laytin <xpor2002@gmail.com>
2023-04-13 00:06:52 +0300

    added entity for security

---
+++ src/main/java/com/laytin/SpringWebApp/security/CustomerDetails.java
@@ -0,0 +1,57 @@
+package com.laytin.SpringWebApp.security;
+
+import com.laytin.SpringWebApp.models.Customer;
+import net.bytebuddy.build.Plugin;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Collections;
+
+public class CustomerDetails implements UserDetails {
+    private final Customer customer;
+
+    public CustomerDetails(Customer customer) {
+        this.customer = customer;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.singletonList(new SimpleGrantedAuthority(customer.getCustomerRole().toString()));
+    }
+
+    @Override
+    public String getPassword() {
+        return this.customer.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return this.customer.getUsername();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+    public Customer getCustomer(){
+        return this.customer;
+    }
+}
+
