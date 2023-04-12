871304091905d59686f750a3b74fef6f0be90cc2
Laytin <xpor2002@gmail.com>
2023-04-13 01:18:17 +0300

    new mappings for registration and login

--- src/main/java/com/laytin/SpringWebApp/controllers/CustomerController.java
+++ src/main/java/com/laytin/SpringWebApp/controllers/CustomerController.java
@@ -5,10 +5,12 @@ import com.laytin.SpringWebApp.services.CustomerService;
 import com.laytin.SpringWebApp.services.OrdService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+
 @Controller
-@RequestMapping("/user")
 public class CustomerController {
     private final CustomerService customerService;
     @Autowired
@@ -16,8 +18,25 @@ public class CustomerController {
         this.customerService = customerService;
     }
     //Show User profile with info and buttons(my address, my orders, my cart)
-    @GetMapping
+    @GetMapping("/user/")
     public String index(){
         return "hello";
+    }
+    @GetMapping("auth/login")
+    public String login(){
+        return "auth/login";
+    }
+    @GetMapping("auth/registration")
+    public String registrationPage(@ModelAttribute("customer") Customer customer) {
+        System.out.println("asd");
+        return "auth/registration";
+    }
+    @PostMapping("auth/registration")
+    public String performRegistration(@ModelAttribute("customer") @Valid Customer customer,
+                                      BindingResult bindingResult) {
+
+        customerService.createCustomer(customer);
+
+        return "redirect:/auth/login";
     }
 }
