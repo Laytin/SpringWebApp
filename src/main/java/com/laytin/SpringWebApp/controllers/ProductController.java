43992c1b2be75bef2994fb8026ceb79ad027f8cf
Laytin <xpor2002@gmail.com>
2023-04-16 23:50:28 +0300

    add controller for main page

--- src/main/java/com/laytin/SpringWebApp/controllers/ProductController.java
+++ src/main/java/com/laytin/SpringWebApp/controllers/ProductController.java
@@ -3,11 +3,11 @@ package com.laytin.SpringWebApp.controllers;
 import com.laytin.SpringWebApp.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
-@RequestMapping()
+@RequestMapping("/products")
 public class ProductController {
     private final ProductService productService;
     @Autowired
@@ -16,7 +16,21 @@ public class ProductController {
     }
     // product list. Main menu
     @GetMapping()
-    public String index(){
-        return "hello";
-    };
+    public String index(@RequestParam(value = "page", required = false,defaultValue = "1") Integer page,
+                        @RequestParam(value = "sort", required = false, defaultValue = "Id") String sorting,
+                        @RequestParam(value = "dir", required = false, defaultValue = "Desc") String direction,
+                        @RequestParam(value = "search", required = false, defaultValue = "") String search,
+                        Model model){
+        if(search.equals("")){
+            model.addAttribute("products",productService.getProducts(page,sorting,direction));
+        }else{
+            model.addAttribute("products",productService.getProducts(page,sorting,direction,search));
+        }
+        return "products/index";
+    }
+    @GetMapping("/{id}")
+    public String show(){
+        return null;
+    }
+
 }
