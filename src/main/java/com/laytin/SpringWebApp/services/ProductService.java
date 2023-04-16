f0868426598c1751fe0b89862123b1f552a71602
Laytin <xpor2002@gmail.com>
2023-04-16 23:51:43 +0300

    catalog

--- src/main/java/com/laytin/SpringWebApp/services/ProductService.java
+++ src/main/java/com/laytin/SpringWebApp/services/ProductService.java
@@ -2,7 +2,6 @@ package com.laytin.SpringWebApp.services;
 
 import com.laytin.SpringWebApp.dao.ProductDAO;
 import com.laytin.SpringWebApp.models.Product;
-import com.laytin.SpringWebApp.repositories.ItemRepository;
 import com.laytin.SpringWebApp.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
@@ -10,34 +9,48 @@ import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @Transactional(readOnly = true)
 public class ProductService {
     private final ProductRepository productRepository;
-    private final ItemRepository itemRepository;
+    private final ProductDAO productDAO;
     @Autowired
-    public ProductService(ProductRepository productRepository, ItemRepository itemRepository) {
+    public ProductService(ProductRepository productRepository, ProductDAO productDAO) {
         this.productRepository = productRepository;
-        this.itemRepository = itemRepository;
+        this.productDAO = productDAO;
     }
     ////////////////////////////////////////////////////////////////
-    public List<Product> getProductList(int page,int perpage){
-        return productRepository.findAll( PageRequest.of(page,perpage)).getContent();
+    //fixed 1+n
+    public List<Product> getProducts(int page,String sort,String dir){
+        System.out.println(dir);
+        Sort.Direction direction = dir.toLowerCase().equals("asc") ? Sort.Direction.ASC : Sort.Direction.DESC;
+        List<Product> productList = productRepository.findAll(PageRequest.of(page-1, 10, Sort.by(direction,sort.toLowerCase()))).getContent();
+
+        productList.stream().forEach(product -> {
+            product.setImageURLs(getImages(product.getId()));
+        });
+        return productList;
     }
-    public List<Product> getProductListSortBy(int page,int perpage, String sortBy){
-        return productRepository.findAll( PageRequest.of(page,perpage, Sort.by(sortBy))).getContent();
+    public List<Product> getProducts(int page,String sort,String dir,String search){
+        Sort.Direction direction = dir.toLowerCase().equals("asc") ? Sort.Direction.ASC : Sort.Direction.DESC;
+
+        List<Product> products = new ArrayList<Product>();
+
+        products.stream().forEach(product -> {
+            product.setImageURLs(getImages(product.getId()));
+        });
+        return null;
     }
 
-    public List<Product> searchProducts(String searchQuest, int page,int perpage){
-        return null;//productDAO.getSearchProducts(searchQuest,page,perpage);
+    private List<String> getImages(int id){
+        List<String> result = new ArrayList<>(Collections.singletonList(
+                "https://www.shutterstock.com/image-vector/default-image-icon-thin-linear-260nw-2136460353.jpg"
+        ));
+        return result;
     }
-    public List<Product> searchProductsSortBy(String searchQuest, int page,int perpage, String sortBy){
-        return null;//productDAO.getSearchProducts(searchQuest,page,perpage);
-    }
-
-    //get product list
-    //get sorted
-    //search (do it with item repo)
 }
