2c8911fcd4f964d65833c8040024dc8f0e1004fe
Laytin <xpor2002@gmail.com>
2023-04-22 03:51:40 +0300

    n+1

--- src/main/java/com/laytin/SpringWebApp/dao/CartDAO.java
+++ src/main/java/com/laytin/SpringWebApp/dao/CartDAO.java
@@ -1,9 +1,14 @@
 package com.laytin.SpringWebApp.dao;
 
+import com.laytin.SpringWebApp.models.CartProduct;
+import org.hibernate.Session;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
+import javax.persistence.Query;
+import java.util.List;
 
 @Component
 public class CartDAO {
@@ -11,5 +16,12 @@ public class CartDAO {
     @Autowired
     public CartDAO(EntityManager entityManager) {
         this.entityManager = entityManager;
+    }
+
+    @Transactional(readOnly = true)
+    public List<CartProduct> getPreloadedCartProducts(int customerId){
+        Session session = entityManager.unwrap(Session.class);
+        Query q = session.createQuery("select c from CartProduct c JOIN FETCH c.product where c.customer.id="+customerId);
+        return q.getResultList();
     }
 }
