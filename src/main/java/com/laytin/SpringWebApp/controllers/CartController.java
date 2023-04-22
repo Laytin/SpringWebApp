3ac25ae26899cb77b90da8f2a9e37dc6232b3bfd
Laytin <xpor2002@gmail.com>
2023-04-22 03:48:25 +0300

    trying to find best solution

--- src/main/java/com/laytin/SpringWebApp/controllers/CartController.java
+++ src/main/java/com/laytin/SpringWebApp/controllers/CartController.java
@@ -1,5 +1,6 @@
 package com.laytin.SpringWebApp.controllers;
 
+import com.laytin.SpringWebApp.models.Address;
 import com.laytin.SpringWebApp.models.CartProduct;
 import com.laytin.SpringWebApp.services.CartService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +8,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/cart")
@@ -19,8 +22,8 @@ public class CartController {
     //return main cart page
     @GetMapping()
     public String index(Model model){
-        model.addAttribute("cart", cartService.getCart());
-        model.addAttribute("cart", cartService.getCart());
+        model.addAttribute("addresses", cartService.getAddresses());
+        model.addAttribute("products", cartService.getCart());
         return "cart/index";
     }
     //change concrete product from cart
@@ -31,12 +34,19 @@ public class CartController {
     }
     //delete product from cart
     @DeleteMapping("/{id}")
-    public String deleteFromCart(@PathVariable("id") int id, @ModelAttribute("cartproduct")CartProduct cartProduct){
+    public String deleteFromCart(@PathVariable("id") int id){
+        System.out.println("asda");
         cartService.deleteCartProduct(id);
         return "redirect:/cart";
     }
-    @PostMapping("/confirm")
-    public String createOrder(BindingResult errors){
+    @PostMapping("/conf")
+    public String createOrder(@ModelAttribute("products") List<CartProduct> products,
+                              @ModelAttribute("address")Address address,
+                              BindingResult errors, Model model){
+/*        cartService.confirmOrder(products,address, errors);
+        if(errors.hasErrors()){
+            return "/cart/index";
+        }*/
         return "redirect:/order";
     }
 
