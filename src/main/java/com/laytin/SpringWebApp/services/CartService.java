f79d2a1f5f324d6785b1e011127f783f6be4156b
Laytin <xpor2002@gmail.com>
2023-04-22 03:49:19 +0300

    order logic, n+1

--- src/main/java/com/laytin/SpringWebApp/services/CartService.java
+++ src/main/java/com/laytin/SpringWebApp/services/CartService.java
@@ -1,45 +1,58 @@
 package com.laytin.SpringWebApp.services;
 
-import com.laytin.SpringWebApp.dto.CartDTO;
-import com.laytin.SpringWebApp.models.Address;
-import com.laytin.SpringWebApp.models.CartProduct;
-import com.laytin.SpringWebApp.models.Customer;
+import com.laytin.SpringWebApp.dao.CartDAO;
+import com.laytin.SpringWebApp.models.*;
 import com.laytin.SpringWebApp.repositories.AddressRepository;
 import com.laytin.SpringWebApp.repositories.CartProductRepository;
+import com.laytin.SpringWebApp.repositories.OrdProductRepository;
+import com.laytin.SpringWebApp.repositories.OrdRepository;
 import com.laytin.SpringWebApp.security.CustomerDetails;
 import org.hibernate.Hibernate;
+import org.hibernate.Session;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.validation.BindingResult;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.Date;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @Transactional(readOnly = true)
 public class CartService {
     private final CartProductRepository cartProductRepository;
     private final AddressRepository addressRepository;
+    private final CartDAO cartDAO;
+    private final OrdRepository ordRepository;
+    private final OrdProductRepository ordProductRepository;
+    @PersistenceContext
+    private final EntityManager entityManager;
 
     @Autowired
-    public CartService(CartProductRepository cartProductRepository, AddressRepository addressRepository) {
+    public CartService(CartProductRepository cartProductRepository, AddressRepository addressRepository, CartDAO cartDAO, EntityManager entityManager, OrdRepository ordRepository, OrdProductRepository ordProductRepository) {
         this.cartProductRepository = cartProductRepository;
         this.addressRepository = addressRepository;
+        this.cartDAO = cartDAO;
+        this.entityManager = entityManager;
+        this.ordRepository = ordRepository;
+        this.ordProductRepository = ordProductRepository;
     }
     //1+n
     @Transactional
-    public CartDTO getCart(){
+    public List<CartProduct> getCart(){
         int customerId = ((CustomerDetails) SecurityContextHolder. getContext(). getAuthentication(). getPrincipal()).getCustomer().getId();
-        List<CartProduct> cartProduct = cartProductRepository.findByCustomerId(customerId);
-        CartDTO cart = new CartDTO(cartProduct);
-        cart.calculateTotal();
-        cartProduct.stream().forEach(cp -> {
+        List<CartProduct> cartProduct = cartDAO.getPreloadedCartProducts(customerId);
+/*        cartProduct.stream().forEach(cp -> {
             if(cp.getQuantity()>cp.getProduct().getQuantity()){
                 cp.setQuantity(cp.getProduct().getQuantity());
                 cartProductRepository.save(cp);
             }
-        });
-        return cart;
+        });*/
+        return cartProduct;
     }
     public List<Address> getAddresses(){
         int customerId = ((CustomerDetails) SecurityContextHolder. getContext(). getAuthentication(). getPrincipal()).getCustomer().getId();
@@ -51,6 +64,7 @@ public class CartService {
         if(cp.getQuantity()==cartProduct.getQuantity())
             return;
 
+        //check that customer can change cartproduct only from their cart
         Hibernate.initialize(cp.getCustomer());
         if(((CustomerDetails) SecurityContextHolder. getContext(). getAuthentication(). getPrincipal()).getCustomer().getId() != cp.getCustomer().getId())
             return;
@@ -64,6 +78,41 @@ public class CartService {
         if(((CustomerDetails) SecurityContextHolder. getContext(). getAuthentication(). getPrincipal()).getCustomer().getId() != cp.getCustomer().getId())
             return;
         cartProductRepository.delete(cp);
+    }
+    @Transactional
+    public void confirmOrder(List<CartProduct> cartRequest, Address choosedAddress,BindingResult result){
+        Session session =entityManager.unwrap(Session.class);
+        Customer principal = ((CustomerDetails) SecurityContextHolder. getContext(). getAuthentication(). getPrincipal()).getCustomer();
+        Customer loadedOwner = session.load(Customer.class, principal.getId());
+
+        //Validating products
+/*
+        cartRequest.stream().forEach(cartProduct -> {
+            cartProductValidator.validate(cartProduct,result);
+        });
+        if(result.hasErrors())
+            return;
+*/
+
+        Ord order =  new Ord();
+        //products binding (2 side)
+        List<OrdProduct> ordProducts = cartRequest.stream().map(cartProduct ->
+                new OrdProduct(order,cartProduct.getProduct(),cartProduct.getQuantity())).collect(Collectors.toList());
+        order.setOrdproducts(ordProducts);
+
+        // customer binding (2 side)
+        Hibernate.initialize(loadedOwner.getOrds());
+        order.setCustomer(loadedOwner);
+        loadedOwner.addOrder(order);
+        //another info
+        order.setAddress(choosedAddress);
+        order.setOrderedAt(new Date());
+        order.setStatus(OrderState.COLLECTING_ORDER);
+
+        ordRepository.save(order);
+        ordProductRepository.saveAll(ordProducts);
+        //if ok, remove count of bought products from avaible quantity
+
     }
 }
 
