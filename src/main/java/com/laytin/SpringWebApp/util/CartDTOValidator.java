e1e68345229f46fb54bdf09b66ca365a4b00bf00
Laytin <xpor2002@gmail.com>
2023-04-26 00:15:21 +0300

    protected from non-fetched objects

--- src/main/java/com/laytin/SpringWebApp/util/CartDTOValidator.java
+++ src/main/java/com/laytin/SpringWebApp/util/CartDTOValidator.java
@@ -2,19 +2,22 @@ package com.laytin.SpringWebApp.util;
 
 import com.laytin.SpringWebApp.dto.CartDTORequest;
 import com.laytin.SpringWebApp.models.CartProduct;
+import com.laytin.SpringWebApp.services.CartService;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
 
-import java.lang.reflect.Field;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 
 @Component
 public class CartDTOValidator implements Validator {
+    private final CartService cartService;
+
+    public CartDTOValidator(CartService cartService) {
+        this.cartService = cartService;
+    }
+
     @Override
     public boolean supports(Class<?> clazz) {
         return CartDTORequest.class.equals(clazz);
@@ -23,8 +26,10 @@ public class CartDTOValidator implements Validator {
     @Override
     public void validate(Object target, Errors errors) {
         CartDTORequest cartDTORequest = (CartDTORequest) target;
+        List<CartProduct> cartProductList = cartService.getCart(); //need recall from db to fetch product quantity(just in case)
         for(int i =0; i<cartDTORequest.getProductList().size(); i++) {
-            CartProduct cp = cartDTORequest.getProductList().get(i);
+            int dtoid = cartDTORequest.getProductList().get(i).getId();
+            CartProduct cp = cartProductList.stream().filter(p -> p.getId()==dtoid).findFirst().get();
             if(cp.getQuantity()>cp.getProduct().getQuantity()){
                 errors.pushNestedPath("productList["+i+"]");
                 errors.rejectValue("quantity", "","Quantity changed of:" + cp.getProduct().getName()+","+cp.getProduct().getColor());
